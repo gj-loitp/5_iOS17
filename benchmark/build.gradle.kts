04feeda026391339ffa006769779877b08c57f3c
roy <roy93group@gmail.com>
2024-04-24 15:28:24 +0700

    update

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -10,11 +10,11 @@ android {
     compileSdk = Versions.COMPILE_SDK
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = "17" }
 
     defaultConfig {
         minSdk = Versions.MIN_SDK
@@ -26,9 +26,7 @@ android {
     flavorDimensions.add("api")
     productFlavors {
         create("apiQ") { dimension = "api" }
-
         create("apiR") { dimension = "api" }
-
         create("apiS") { dimension = "api" }
     }
 
